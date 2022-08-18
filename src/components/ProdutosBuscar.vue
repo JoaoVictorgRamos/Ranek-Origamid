e4626390af22ddb674e908e6b173129f9eac76c0
JoaoVictogRamos <joaovictorgramos12@gmail.com>
2022-08-18 10:26:55 -0300

    home

---
+++ src/components/ProdutosBuscar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Protudos Buscar</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/components/ProdutosLista.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Protudos Lista</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
