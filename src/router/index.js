80344c18f19b1157d7c533c31773751869f98720
JoaoVictogRamos <joaovictorgramos12@gmail.com>
2022-08-20 12:47:51 -0300

    login

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomeView from "../views/HomeView.vue";
 import ProdutoView from "../views/ProdutoView.vue";
+import LoginView from "../views/LoginView.vue";
 
 const routes = [
   {
@@ -13,6 +14,12 @@ const routes = [
     path: "/produto/:id",
     name: "ProdutoView",
     component: ProdutoView,
+    props: true,
+  },
+  {
+    path: "/LoginView",
+    name: "LoginView",
+    component: LoginView,
     props: true,
   },
 ];
