4fd55c497883da67debdb40977af95aa57a09886
JoaoVictogRamos <joaovictorgramos12@gmail.com>
2022-08-21 23:55:57 -0300

    usuario-atualizar

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -19,7 +19,7 @@ export default {
   name: "TheHeader",
   computed: {
     nome() {
-      return this.$store.state.usuario.nome;
+      return this.$store.state.usuario.nome.replace(/ .*/, "");
     },
   },
 };
--- src/views/usuario/UsuarioEditar.vue
+++ src/views/usuario/UsuarioEditar.vue
@@ -1,10 +1,37 @@
 <template>
-  <p>Usuario Editar</p>
+  <section>
+    <UsarioForm>
+      <button class="btn" @click.prevent="atualizarUsuario">
+        Atualizar Usuario
+      </button>
+    </UsarioForm>
+  </section>
 </template>
 
 <script>
+import UsarioForm from "@/components/UsuarioForm.vue";
+import { api } from "@/axios/services.js";
+
 export default {
   name: "UsuarioEditar",
+  components: {
+    UsarioForm,
+  },
+  methods: {
+    atualizarUsuario() {
+      api
+        .put(
+          `/usuario/${this.$store.state.usuario.id}`,
+          this.$store.state.usuario
+        )
+        .then(() => {
+          this.$store.dispatch("getUsuario");
+          this.$router.push({ name: "usuario" }).catch((error) => {
+            console.log(error.response);
+          });
+        });
+    },
+  },
 };
 </script>
 
