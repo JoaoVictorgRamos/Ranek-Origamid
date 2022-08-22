2023455efaaf7999983d596323501862f6b73303
JoaoVictogRamos <joaovictorgramos12@gmail.com>
2022-08-21 22:56:35 -0300

    produto-adicionar

--- src/components/ProdutoAdicionar.vue
+++ src/components/ProdutoAdicionar.vue
@@ -1,11 +1,59 @@
 <template>
-  <p>ProdutoAdicionar</p>
+  <form class="adicionar-produto">
+    <label for="nome">Nome</label>
+    <input type="text" id="nome" name="nome" v-model="produto.nome" />
+    <label for="preco">Preço 💰</label>
+    <input type="number" id="preco" name="preco" v-model="produto.preco" />
+    <label for="fotos">Fotos 📷</label>
+    <input type="file" id="fotos" name="fotos" ref="fotos" />
+    <label for="fotos">Descrição 📄</label>
+    <textarea id="preco" name="preco" v-model="produto.descricao"></textarea>
+    <input
+      class="btn"
+      type="button"
+      value="Adicionar Produto"
+      @click.prevent="adicionarProduto"
+    />
+  </form>
 </template>
 
 <script>
+import { api } from "@/axios/services.js";
+
 export default {
   name: "ProdutoAdicionar",
+  data() {
+    return {
+      produto: {
+        nome: "",
+        preco: "",
+        descricao: "",
+      },
+    };
+  },
+  methods: {
+    formatarProduto() {
+      this.produto.usuario_id = this.$store.state.usuario.id;
+    },
+    adicionarProduto() {
+      this.formatarProduto();
+      api.post("/produto", this.produto).then(() => {
+        this.$store.dispatch("getUsuarioProdutos");
+      });
+    },
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.adicionar-produto {
+  display: grid;
+  grid-template-columns: 100px 1fr;
+  align-items: center;
+  margin-bottom: 60px;
+}
+
+.btn {
+  grid-column: 2;
+}
+</style>
